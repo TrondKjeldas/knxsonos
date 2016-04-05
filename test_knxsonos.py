f44929d6b2a24c054b3b060476110550b368c043
Trond Kjeldås <trond@kjeldas.no>
2016-04-05 20:01:51 +0200

    Testing Travis CI

---
+++ test_knxsonos.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestKnxSonos(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+
+        pass
+
+if __name__ == '__m':
+    unittest.main()
