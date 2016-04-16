ecf1e4c08a44164be43300f4cc17ca17250be8fb
Trond Kjeldås <trond@kjeldas.no>
2016-04-16 17:25:02 +0200

    Corrected license info.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.1',
+    version='0.2.2',
 
     description='Control a Sonos system from your KNX installation',
     long_description=long_description,
@@ -54,7 +54,7 @@ setup(
         'Topic :: Home Automation',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
